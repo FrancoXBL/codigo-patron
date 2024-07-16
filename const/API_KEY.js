06bdba1494714df971c0ccc42b2d6e2776f3f10a
FranXBL <francoxavierbonin@hotmail.com>
2024-07-16 12:47:17 -0300

    cambios a apikey

--- const/API_KEY.js
+++ const/API_KEY.js
@@ -4,6 +4,7 @@ function returnApiKey() {
   if (process.env.NODE_ENV === "development") {
     return "http://localhost:3000";
   } else {
-    return "http://codigopatron.com.ar";
+    // return "http://codigopatron.com.ar";
+    return "https://codigo-patron-backend-production.up.railway.app";
   }
 }
